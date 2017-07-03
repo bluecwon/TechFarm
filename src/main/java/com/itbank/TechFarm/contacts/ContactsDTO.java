6dfefb3ea5f0ad77750a21e820a82b5fd61152be
aker4m <mageaker@hotmail.com>
2017-07-03 21:50:29 +0900

    170703 연락처 DTO 파일

---
+++ src/main/java/com/itbank/TechFarm/contacts/ContactsDTO.java
@@ -0,0 +1,86 @@
+package com.itbank.TechFarm.contacts;
+
+public class ContactsDTO {
+	private String photo;
+	private String name;
+	private String company;
+	private String jobtitle;
+	private String email;
+	private String phone;
+	private String notes;
+	
+	private String address;
+	private String birthday;
+	private String website;
+	private String relationship;
+	
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getJobtitle() {
+		return jobtitle;
+	}
+	public void setJobtitle(String jobtitle) {
+		this.jobtitle = jobtitle;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getRelationship() {
+		return relationship;
+	}
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+	
+	
+	
+}
