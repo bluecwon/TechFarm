fe2babb02e49d514fe2d98044e11dbcd34715946
bluecwon <blue_cwon@naver.com>
2017-07-04 15:45:50 +0900

    170704 search Init

--- src/main/java/com/itbank/TechFarm/HomeController.java
+++ src/main/java/com/itbank/TechFarm/HomeController.java
@@ -1,20 +1,27 @@
 package com.itbank.TechFarm;
 
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+import com.itbank.TechFarm.search.SearchNaver;
+
 @Controller
 public class HomeController {
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private SearchNaver sn;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "home";
@@ -23,6 +30,24 @@ public class HomeController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Locale locale, Model model) {
 		return "login/login";
+	}
+	
+	@RequestMapping(value = "/search.naver", method = RequestMethod.POST)
+	public ModelAndView home(HttpServletRequest request, HttpServletResponse response) {
+		String query=request.getParameter("search");
+		System.out.println(query);
+		int pageNum=ServletRequestUtils.getIntParameter(request, "pagenum",0);
+		System.out.println(pageNum);
+		pageNum=pageNum*10+1;
+		System.out.println(pageNum);
+		List<Object> list=sn.search("webkr", query, String.valueOf(pageNum));
+		Object total=(Object)list.get(0);
+		Object time=(Object)list.get(1);
+		ModelAndView mav=new ModelAndView("search/searchresult","result",list.subList(2, list.size()));
+		mav.addObject("time", time);
+		mav.addObject("total", total);
+		mav.addObject("search",query);
+		return mav;
 	}
 	
 }
