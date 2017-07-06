54528351699c5f3dce69c6ee9c93b959e37a93c2
bluecwon <blue_cwon@naver.com>
2017-07-06 19:29:28 +0900

    170706

--- src/main/java/com/itbank/TechFarm/tchat/websocket/ChatWebSocketHandler.java
+++ src/main/java/com/itbank/TechFarm/tchat/websocket/ChatWebSocketHandler.java
@@ -13,8 +13,7 @@ public class ChatWebSocketHandler extends TextWebSocketHandler{
 	private Map<String, WebSocketSession> users = new ConcurrentHashMap<String, WebSocketSession>();
 
 	@Override
-	public void afterConnectionEstablished(
-			WebSocketSession session) throws Exception {
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		log(session.getId() + " 연결 됨");
 		users.put(session.getId(), session);
 	}
